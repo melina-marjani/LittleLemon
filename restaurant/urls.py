13d02180f2f60718db5eb21b4af918a3d26d2808
melinamarjani <melina.marjani@duke.edu>
2025-07-29 11:16:19 -0400

    added APIs

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('', views.index, name='home'),
+    path('menu/items/', views.MenuItemsView.as_view(), name='menu-items'),
+    path('menu/items/<int:pk>/', views.SingleMenuItemView.as_view(), name='single-menu-item'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Menu
+from .serializers import MenuSerializer
+from rest_framework import viewsets
+from .models import Booking
+from .serializers import BookingSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
 
 def index(request):
     return render(request, 'index.html')
